edfb2095eb352793a457e2aba6dc676a5a424229
Francieverton <francieverton.so@gmail.com>
2026-03-31 20:19:51 -0300

    learning the syntax

--- ex1.py
+++ ex1.py
@@ -1,4 +1,14 @@
-nome = "Francis"
+meta = 0.05
+taxa = 0
+rendeu = 0.30
 
-nome = 72429487
-print(type(nome))
+if rendeu > 0.20:
+    taxa = 0.04
+    print('Taxa de {}, pq o valor que rendeu é maior que 20%.'.format(taxa))
+
+elif rendeu > meta:
+    taxa = 0.02
+    print('Taxa de apenas {}.'.format(taxa))
+
+else:
+    print('sem taxa')
