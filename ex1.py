a70a5dde2aa324f517291b6952fcfb7df9199e67
Francieverton <francieverton.so@gmail.com>
2026-04-02 16:30:30 -0300

    List and index

--- ex1.py
+++ ex1.py
@@ -1,12 +1,10 @@
-nome = input('Digite seu primeiro nome: ')
-email = input('Digite seu melhor email: ')
+produtos = ['tv', 'celular', 'geladeira', 'forno', 'computador']
+vendas = [1000, 1500, 2000, 3000, 5000]
 
-#vendo se tem o email é válido
-if nome and email:
-    pos_a = email.find('@')
-    servidor = email[pos_a:]
-    if pos_a != -1 and '.' in servidor:
-        print('Cadastro concluído!')
+mais_vendido = vendas.index(max(vendas))
+menos_vendido = vendas.index(min(vendas))
 
-else:
-    print('Digite as informações corretamente!')
+mais_vendido = produtos[mais_vendido]
+menos_vendido = produtos[menos_vendido]
+
+print('O produto mais vendido é {}, e o menos vendido é {}'.format(mais_vendido, menos_vendido))
